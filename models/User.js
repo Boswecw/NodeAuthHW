dbdef78a92d0214914f87f37264c3154c3dd180b
Charles W Boswell <charliewboswell@gmail.com>
2025-06-22 22:28:06 -0400

    second push

--- models/User.js
+++ models/User.js
@@ -1,33 +1,55 @@
-const mongoose = require('mongoose');
-const { isEmail } = require('validator');
+const User = require("../models/User");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, 'Please enter an email'],
-    unique: true,
-    lowercase: true,
-    validate: [isEmail, 'Please enter a valid email']
-  },
-  password: {
-    type: String,
-    required: [true, 'Please enter a password'],
-    minlength: [6, 'Minimum password length is 6 characters'],
+// handle errors
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: '', password: '' };
+
+  // duplicate email error
+  if (err.code === 11000) {
+    errors.email = 'that email is already registered';
+    return errors;
   }
-});
 
-// fire a function after doc saved to db
-userSchema.post('save', function (doc, next) {
-  console.log('new user was created & saved', doc);
-  next();
-});
+  // validation errors
+  if (err.message.includes('user validation failed')) {
+    // console.log(err);
+    Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(val);
+      // console.log(properties);
+      errors[properties.path] = properties.message;
+    });
+  }
 
-// fire a function before doc saved to db
-userSchema.pre('save', function (next) {
-  console.log('user about to be created & saved', this);
-  next();
-});
+  return errors;
+}
 
-const User = mongoose.model('user', userSchema);
+// controller actions
+module.exports.signup_get = (req, res) => {
+  res.render('signup');
+}
 
-module.exports = User;
+module.exports.login_get = (req, res) => {
+  res.render('login');
+}
+
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  }
+  catch(err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+ 
+}
+
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
+  res.send('user login');
+}
